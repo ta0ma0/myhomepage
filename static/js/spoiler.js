b09fd4ff8a012ab9d6c4953f385955d73237192c
Ruslan from Legion <ruslanakmanov@beget.ru>
2021-10-23 02:16:12 +0300

    skills, spoiler

---
+++ static/js/spoiler.js
@@ -0,0 +1,18 @@
+function Spoiler() {
+    var ele = document.getElementById("contentSpoiler");
+    var text = document.getElementById("linkSpoiler");
+    var table_tag = document.getElementById("top_after_spoiler")
+    if (ele.style.display == "block") {
+        ele.style.display = "none";
+        text.innerHTML = "https://ruslanakmanov.link/expirience";
+        table_tag.style.textAlign = "left"
+        table_tag.style.verticalAlign = "middle"
+    } else {
+        ele.style.display = "block";
+        text.innerHTML = "Hide &#172\;";
+        table_tag.style.textAlign = "left"
+        table_tag.style.verticalAlign = "middle"
+
+
+    }
+}
