ff1c06368be801c920095161d6867003c370a1fa
Ruslan from Legion <ruslanakmanov@beget.ru>
2021-10-23 15:07:43 +0300

    about me spoiler

--- static/js/spoiler.js
+++ static/js/spoiler.js
@@ -14,3 +14,20 @@ function Spoiler() {
         table_tag.style.verticalAlign = "top";
     }
 }
+
+function Spoiler2() {
+    var ele = document.getElementById("contentSpoiler2");
+    var text = document.getElementById("linkSpoiler2");
+    var table_tag = document.getElementById("top_after_spoiler2")
+    if (ele.style.display == "block") {
+        ele.style.display = "none";
+        text.innerHTML = "https://ruslanakmanov.link/about";
+        table_tag.style.textAlign = "left"
+        table_tag.style.verticalAlign = "middle"
+    } else {
+        ele.style.display = "block";
+        text.innerHTML = "Hide &#172\;";
+        table_tag.style.textAlign = "left";
+        table_tag.style.verticalAlign = "top";
+    }
+}
